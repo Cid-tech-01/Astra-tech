8e725390b66c18f1651b70789d3dadab0ef8f584
Cid-tech-01 <gamerinsaan6@gmail.com>
2026-05-24 09:42:32 +0530

    Table Calculator V1

--- loop.js
+++ loop.js
@@ -1,11 +1,16 @@
-const table = 2;
-console.log(table * 1);
-console.log(table * 2);
-console.log(table * 3);
-console.log(table * 4);
-console.log(table * 5);
-console.log(table * 6);
-console.log(table * 7);
-console.log(table * 8);
-console.log(table * 9);
-console.log(table * 10);
+const prompt = require("prompt-sync")({ sigint: true });
+const tableOf = prompt("Enter the number to Calculate Table: ");
+const tableUpto = prompt("Enter the limit of Table: ");
+// console.log(table * 1);
+// console.log(table * 2);
+// console.log(table * 3);
+// console.log(table * 4);
+// console.log(table * 5);
+// console.log(table * 6);
+// console.log(table * 7);
+// console.log(table * 8);
+// console.log(table * 9);
+// console.log(table * 10);
+for (let i = 1; i <= tableUpto; i++) {
+  console.log(tableOf * i);
+}
