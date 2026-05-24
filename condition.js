5ddd6744a5db305d7a6129029261a4adbeb1ef79
Cid-tech-01 <gamerinsaan6@gmail.com>
2026-05-24 09:18:29 +0530

    Basic condition program

---
+++ condition.js
@@ -0,0 +1,11 @@
+//if else conditioning//
+const age=32;
+if(age<18){
+    console.log("Not eligible:Liscense not approved");
+}
+else if(age>=18 && age<=60){
+    console.log("Eligible; Liscenese approaved");
+}
+else{
+    console.log("Not eligible; DEAD");
+}
